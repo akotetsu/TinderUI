83f7b3fd66c2eaf44bb4476c685f4eca684fbbde
akotetsu <yuanliqu@gmail.com>
2025-07-13 20:19:39 +0900

    Initial Commit

---
+++ TinderWithSwiftUI/TinderWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderWithSwiftUIApp.swift
+//  TinderWithSwiftUI
+//
+//  Created by 原里駆 on 2025/07/13.
+//
+
+import SwiftUI
+
+@main
+struct TinderWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
